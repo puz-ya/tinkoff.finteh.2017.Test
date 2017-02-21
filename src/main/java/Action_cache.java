a529b16fdbd27bdb8ba1eaf103e96ab25fedd394
puz_ya <puzino.y.a@gmail.com>
2017-02-21 19:46:08 +0300

    Added checkFile, loadData, searchRate; Fix in Main - code to getFromWeb().

--- src/main/java/Action_cache.java
+++ src/main/java/Action_cache.java
@@ -1,3 +1,9 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Puzino Yury on 20.02.2017.
  * work with text file ("cache") and store / update currencies rates
@@ -7,6 +13,10 @@ public class Action_cache {
     private String mFrom;
     private String mTo;
     private double mRate;
+
+    private final String FILE_NAME = "puzino_currconv.txt";
+    private String[] fileContent;
+    List<String> mStringList;
 
     public Action_cache(String from, String to){
         this.mFrom = from;
@@ -25,20 +35,237 @@ public class Action_cache {
         return mRate;
     }
 
+    /** update rate for current pair from-to
+     * */
+    public void setRate(double rate){
+        this.mRate = rate;
+    }
+
     /** check file for from-to pairs, check dates.
      * if outdated => connect to site
      * @return b - (false) if need web update, (true) data is ok
      * */
-    public boolean check(){
+    public boolean checkFile(){
+
+        File file = new File(FILE_NAME);
+        if(!file.exists()){
+
+            //creating cache file
+            try {
+                if (!file.createNewFile()) {
+                    //Main.show("File already exists!");
+                    //if file exists, go ahead and read it
+                    return true;
+                }else {
+                    //file was created, it's empty now, need to fill it with data, check failed
+                    return false;
+                }
+            }catch (IOException ex){
+                Main.show("Sorry: Could not create cache file, check your write privileges.");
+                return false;
+            }
+        }
+
+        //load all from file
+        boolean bLoad = loadDataFromFile();
+        if(!bLoad){return false;}
+
+        //search rate from query
+        boolean bSearch = searchRate();
+        if(!bSearch){return false;}
+
+        return true;
+    }
+
+    /** Load values from local file */
+    public boolean loadDataFromFile(){
+
+        try {
+
+            // It's even easier in Java 8
+            mStringList = Files.readAllLines(new File(FILE_NAME).toPath());
+
+            /* {{{ Code for example, but it's working too }}}
+
+            InputStream is = new FileInputStream(FILE_NAME);
+            BufferedReader buf = new BufferedReader(new InputStreamReader(is));
+
+            StringBuilder sb = new StringBuilder();
+            String line = buf.readLine();
+
+            while(line != null){
+                sb.append(line).append("\n");
+                line = buf.readLine();
+            }
+
+            String fileAsString = sb.toString();
+            System.out.println("Contents : " + fileAsString);
+
+            is.close();
+            */
+        } catch (IOException e) {
+            Main.show("Sorry: I/O error while loading cache.");
+        }
+
+        if(mStringList.isEmpty()){
+           //nothing to show, file was empty
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean searchRate(){
+        List<String[]> stringLists = new ArrayList<>();
+
+        //mStringList not empty (checked before)
+        for(String sTmp : mStringList){
+
+            //parse each line
+            String[] sArray = sTmp.split("\\|");
+
+            if(sArray.length != 3){
+                //wrong data, cache was corrupted
+                Main.show("Error: cache data was corrupted.");
+                return false;
+            }
+
+            stringLists.add(sArray);
+        }
+
+        String date;
+        Double rate;
+
+        for(String[] sArray : stringLists){
+            String sCurr1 = getFrom() + "-" + getTo();
+            String sCurr2 = getTo() + "-" + getFrom();
+
+            if(sArray[0].equals(sCurr1)){
+                date = sArray[1];
+                try{
+                    rate = Double.parseDouble(sArray[2]);
+                }catch (Exception ex){
+                    Main.show("Incorrect rate in cache");
+                    return false;
+                }
+                //rate was founded
+
+                //check cache currency date
+                LocalDate localDateToday = LocalDate.now();
+                LocalDate localDate = LocalDate.parse( date );
+                if(localDateToday.getYear() != localDate.getYear() ||
+                        localDateToday.getMonthValue() != localDate.getMonthValue() ||
+                        localDateToday.getDayOfMonth() - localDate.getDayOfMonth() > 0){
+                    //cache is out of date, delete this object, rewrite cache, send update command
+                    stringLists.remove(sArray);
+                    rewriteCache(stringLists);
+                    return false;
+                }
+
+                //everything is ok, cache is up-to-date
+                setRate(rate);
+                return true;
+            }
+
+            //reverse currency
+            if(sArray[0].equals(sCurr2)){
+                date = sArray[1];
+                try {
+                    rate = 1.0 / Double.parseDouble(sArray[2]);
+                }catch (Exception ex){
+                    Main.show("Incorrect rate in cache");
+                    return false;
+                }
+                //check cache currency date
+                LocalDate localDateToday = LocalDate.now();
+                LocalDate localDate = LocalDate.parse( date );
+                if(localDateToday.getYear() != localDate.getYear() ||
+                        localDateToday.getMonthValue() != localDate.getMonthValue() ||
+                        localDateToday.getDayOfMonth() - localDate.getDayOfMonth() > 0){
+                    //cache is out of date, delete this object, rewrite cache, send update command
+                    stringLists.remove(sArray);
+                    rewriteCache(stringLists);
+                    return false;
+                }
+
+                //everything is ok, cache is up-to-date
+                setRate(rate);
+                return true;
+            }
+        }
+
+
         return false;
     }
 
-    /** update rate for current pair from-to
-     * */
-    public void update(double rate){
-        this.mRate = rate;
+    /** Save names & descriptions to local file */
+    /*
+    boolean SaveDataToFile(String from, String to, String date, Double rate){
 
-        //insert into file
+        name = name.replace("\n", " ");
+
+        // split into 2 parts - name & meaning
+        int limit = name.indexOf("(");
+        if(limit == -1){	//Without a meaning - Orcs, Tieflings...
+            limit = name.length();
+        }
+        String newname = name.substring(0, limit);
+        String meaning = name.substring(limit, name.length());
+
+        //Name, Meaning, Race, Gender
+        String input_string = "<b>"+newname+"</b><br />"+meaning+"<br />"+race+", "+gender+".";
+
+        //Data save process
+        try {
+            FileOutputStream outputStream = ctx.openFileOutput(FILE_NAME, Context.MODE_APPEND);
+            DataOutputStream out = new DataOutputStream(outputStream);
+
+            //In pro-version
+            out.writeUTF(input_string);
+            out.flush();
+            out.close();
+
+        } catch (IOException e) {
+            Log.i("Data Input", "I/O Error SaveToFile");
+            return false;
+        }
+
+        return true;
 
     }
+    */
+
+    /** Delete value with Number "num" */
+    /*
+    void DeleteValue(int num){
+
+        String[] tmp = loadDataFromFile();
+
+        if (num > tmp.length) return;
+
+        FileOutputStream outputStream;
+
+        try {
+            //Rewrite file
+            outputStream = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            DataOutputStream out = new DataOutputStream(outputStream);
+            out.writeUTF("");
+            out.flush();
+            out.close();
+
+            outputStream = openFileOutput(FILE_NAME, MODE_APPEND);
+            out = new DataOutputStream(outputStream);
+            for (int i=0; i<tmp.length; i++){
+                if (i != num){
+                    out.writeUTF(tmp[i]+".");
+                }
+            }
+            out.flush();
+            out.close();
+
+        } catch (IOException e) {
+            Main.show("Data Input : I/O Error Delete");
+        }
+    }
+    //*/
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,83 +13,102 @@ public class Main {
         Welcome welcome = new Welcome();
         welcome.enterCurrencies();
 
-        String sJSON;
-        double dRate;
-
         //check our cache file (or create new one)
         Action_cache action_cache = new Action_cache(welcome.getmFromCurrency(),welcome.getmToCurrency());
-        boolean bIsNew = action_cache.check();
+
+        //if file not exist - false, getFromWeb + try to set cache again
+        //if file was created - false, getFromWeb + update
+        //if file existed - true, loadDataFromFile
+
+        boolean bIsNew = action_cache.checkFile();
 
         if(!bIsNew) {
 
-            ExecutorService executorService = Executors.newSingleThreadExecutor();
-
-            Future<String> future1 = executorService.submit(
-                    //init connect class object & get data from site
-                    new Action_connect(welcome.getmFromCurrency(), welcome.getmToCurrency())
-            );
-
-            //async waiting and drawing
-            System.out.print("Connecting.");
-            while (!future1.isDone()) {
-                System.out.print(".");  //draw dots in command line
-                try {
-                    Thread.sleep(50); //sleep for 100 millisecond before checking again
-                } catch (Exception e) {
-                    //some action_connect error
-                    executorService.shutdown();
-                    show("Sorry: error in the thread.");
-                    return;
-                }
-            }
-            show("");   //new line
-
-            //get throws callable
-            try {
-                //get values from the future ... hehe
-                sJSON = future1.get();
-            }catch (ExecutionException|InterruptedException e) {
-                executorService.shutdown();
-                return;
-            }
-
-            Future<Double> future2 = executorService.submit(
-                //init connect class object & get data from site
-                new Action_parse(sJSON)
-            );
-
-            //async waiting and drawing
-            System.out.print("Parsing.");
-            while (!future2.isDone()) {
-                System.out.print(".");  //draw dots in command line
-                try {
-                    Thread.sleep(50); //sleep for 10 millisecond before checking again
-                } catch (Exception e) {
-                    //some action_connect error
-                    executorService.shutdown();
-                    show("Sorry: error in the thread.");
-                    return;
-                }
-            }
-            show("");   //new line
-
-            try {
-                //get values from the future ... again
-                dRate = future2.get();
-            }catch (ExecutionException|InterruptedException e) {
-                executorService.shutdown();
-                return;
-            }
-
-            //show result
-            show(welcome.getmFromCurrency() + " => " + welcome.getmToCurrency() + " : " + dRate);
-
-            executorService.shutdown();
+            getFromWeb(welcome.getmFromCurrency(), welcome.getmToCurrency());
 
         }else{
             //our file data (cache) is up-to-date, show it
             show(action_cache.getFrom() + " => " + action_cache.getTo() + " : " + action_cache.getRate());
         }
+    }
+
+    /**
+     *
+     * */
+    public static void getFromWeb(String sFrom, String sTo){
+
+        String sJSON;
+        double dRate;
+
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+        Future<String> future1 = executorService.submit(
+                //init connect class object & get data from site
+                new Action_connect(sFrom, sTo)
+        );
+
+        //async waiting and drawing
+        System.out.print("Connecting.");
+        while (!future1.isDone()) {
+            System.out.print(".");  //draw dots in command line
+            try {
+                Thread.sleep(50); //sleep for 100 millisecond before checking again
+            } catch (Exception e) {
+                //some action_connect error
+                executorService.shutdown();
+                show("Sorry: error in the thread.");
+                return;
+            }
+        }
+        show("");   //new line
+
+        //get throws callable
+        try {
+            //get values from the future ... hehe
+            sJSON = future1.get();
+        }catch (ExecutionException|InterruptedException e) {
+            executorService.shutdown();
+            return;
+        }
+
+        Future<Double> future2 = executorService.submit(
+                //init connect class object & get data from site
+                new Action_parse(sJSON)
+        );
+
+        //async waiting and drawing
+        System.out.print("Parsing.");
+        while (!future2.isDone()) {
+            System.out.print(".");  //draw dots in command line
+            try {
+                Thread.sleep(50); //sleep for 10 millisecond before checking again
+            } catch (Exception e) {
+                //some action_connect error
+                executorService.shutdown();
+                show("Sorry: error in the thread.");
+                return;
+            }
+        }
+        show("");   //new line
+
+        try {
+            //get values from the future ... again
+            dRate = future2.get();
+        }catch (ExecutionException|InterruptedException e) {
+            executorService.shutdown();
+            return;
+        }
+
+        //shutdown service
+        executorService.shutdown();
+
+        //TODO: update cache
+        //action_cache.updateRate(dRate);
+        //action_cache.update();
+
+        //show result
+        show(sFrom + " => " + sTo + " : " + dRate);
+
     }
 
     /** too lazy to write System... many times
